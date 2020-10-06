17cb8f8beff8453c732c3e24b90de0c20e3d9c62
Brendan Folie <bfolie@citrine.io>
2020-10-06 09:46:53 -0700

    Test python 3.8, add version

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ import setuptools
 
 # Package meta-data.
 NAME = "smlb"
+VERSION = "0.0.0"
 DESCRIPTION = "Scientific Machine Learning Benchmark"
 URL = "https://github.com/CitrineInformatics/smlb"
 EMAIL = "mrupp@mrupp.io"
@@ -41,7 +42,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name=NAME,
-    # version=VERSION, # taken from git tags
+    version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
