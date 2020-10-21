343820d8e24078553c8b8c424dcba3ddaf225da4
Brendan Folie <bfolie@citrine.io>
2020-10-21 08:08:48 -0700

    Reformat with Black and change some imports to provide examples

--- tests/workflows/test_optimization_workflow.py
+++ tests/workflows/test_optimization_workflow.py
@@ -14,22 +14,22 @@ import smlb
 
 def test_optimization_trajectories():
     """Ensure that a simple optimization workflow can be run."""
-    from smlb.datasets.synthetic.friedman_1979.friedman_1979 import Friedman1979Data
+    from smlb.datasets.synthetic import Friedman1979Data
     dataset = Friedman1979Data(dimensions=5)
     sampler = smlb.RandomVectorSampler(size=100, rng=0)
     training_data = sampler.fit(dataset).apply(dataset)
 
-    from smlb.learners.scikit_learn.random_forest_regression_sklearn import RandomForestRegressionSklearn
+    from smlb.learners import RandomForestRegressionSklearn
 
     learner = RandomForestRegressionSklearn(uncertainties="naive", rng=0)
     learner.fit(training_data)
 
     pi_scorer = smlb.ProbabilityOfImprovement(target=2, goal="minimize")
 
-    from smlb.optimizers.random_optimizer import RandomOptimizer
+    from smlb.optimizers import RandomOptimizer
     optimizer = RandomOptimizer(num_samples=30, rng=0)
 
-    from smlb.workflows.optimization_trajectory_comparison import OptimizationTrajectoryComparison
+    from smlb.workflows import OptimizationTrajectoryComparison
     workflow = OptimizationTrajectoryComparison(
         data=dataset,
         model=learner,
