4f34dca2de7ad8487824ee0204ebac94cfb2ba54
Brendan Folie <bfolie@citrine.io>
2021-06-21 14:34:36 -0700

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import setuptools
 
 # Package meta-data.
 NAME = "smlb"
-VERSION = "0.3.2"
+VERSION = "0.3.3"
 DESCRIPTION = "Scientific Machine Learning Benchmark"
 URL = "https://github.com/CitrineInformatics/smlb"
 EMAIL = "mrupp@mrupp.io"
