94aaf2f07b059d10727438f60655abd53979c2bc
Ryan Latture <rlatture@citrine.io>
2021-07-22 13:08:30 -0700

    Added pca transformations.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import setuptools
 
 # Package meta-data.
 NAME = "smlb"
-VERSION = "0.5.0"
+VERSION = "0.6.0"
 DESCRIPTION = "Scientific Machine Learning Benchmark"
 URL = "https://github.com/CitrineInformatics/smlb"
 EMAIL = "mrupp@mrupp.io"
