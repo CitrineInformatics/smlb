d81e0caa15120d03a2c9cc9bccb031e69b33fed8
Ryan Latture <rlatture@citrine.io>
2021-09-14 09:35:14 -0700

    Added RFECV from sklearn.

--- smlb/feature_selection/__init__.py
+++ smlb/feature_selection/__init__.py
@@ -1,4 +1,5 @@
 from .rfe_sklearn import RFESklearn
+from .rfecv_sklearn import RFECVSklearn
 from .select_from_model_sklearn import SelectFromModelSklearn
 from .select_from_total_importance import SelectFromTotalImportance
 from .select_percentile_sklearn import SelectPercentileSklearn
